7b2b9de2c35002d0ba1d422db468cbac8e27abb4
vivek6219 <vivek6219@gmail.com>
2026-06-27 12:11:03 -0700

    first commit

---
+++ dedupe-engine/src/main/java/dev/vivek6219/dedupeengine/DedupeEngineApplication.java
@@ -0,0 +1,13 @@
+package dev.vivek6219.dedupeengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DedupeEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DedupeEngineApplication.class, args);
+	}
+
+}
---
+++ dedupe-engine/src/test/java/dev/vivek6219/dedupeengine/DedupeEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vivek6219.dedupeengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DedupeEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
